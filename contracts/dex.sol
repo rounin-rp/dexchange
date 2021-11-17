0ac25e3bd0192490da9b8207f0dbb1d7ece4e04a
= <=>
2021-11-17 14:17:00 +0530

    added market buy and sell function

--- contracts/dex.sol
+++ contracts/dex.sol
@@ -70,55 +70,102 @@ contract Dex is Wallet{
     function getOrderBook(bytes32 _ticker, uint _type)public view returns(Order[] memory){
         return orderBook[_ticker][_type];
     }
-
-    function createMarketOrder(uint _orderType, bytes32 _ticker, uint _amount)external returns(bool result){
+    function createMarketOrder(uint _orderType, bytes32 _ticker, uint _amount)external{
         uint otype = _orderType ^ 1;
-        Order[] storage orders = orderBook[_ticker][otype];
         if(otype == 1){
-            uint index = 0;
-            uint totalCost = 0;
-            uint totalToken = 0;
-            uint buyerEth = balances[msg.sender][ethTicker];
-            uint sellerToken = 0;
-            while(index < orders.length && _amount > 0){
-                Order storage order = orders[index];
-                sellerToken = balances[order.trader][_ticker];
-                if(sellerToken < _amount){
-                    index++;
-                    continue;
-                }
-                if(order.amount >= _amount){
-                    if(buyerEth >= _amount * order.price){
-                        balances[order.trader][_ticker]-= _amount;
-                        balances[order.trader][ethTicker]+= _amount * order.price;
-                        order.amount = order.amount - _amount;
-                        totalCost+= _amount * order.price;
-                        totalToken+= _amount;
-                        buyerEth-=_amount * order.price;
-                        _amount = 0;
-                        result = true;
-                        break;
-                    }
-                }
-                else{
-                    if(buyerEth >= order.amount * order.price){
-                        balances[order.trader][_ticker]-=order.amount;
-                        balances[order.trader][ethTicker]+= order.amount * order.price;
-                        _amount-= order.amount;
-                        totalCost+= order.amount * order.price;
-                        totalToken+= order.amount;
-                        buyerEth-= order.amount * order.price;
-                        order.amount = 0;
-                    }
-                }
-                index++;
+            _executeSell(_ticker, otype, _amount);
+        }
+        else{
+            require(balances[msg.sender][_ticker] >= _amount,"Not enough tokens to sell");
+            _executeBuy(_ticker, otype, _amount);
+        }
+    }
+    function _executeBuy(bytes32 _ticker, uint otype, uint _amount)private{
+        //msg.sender is seller and buyer is in buy side of the orderbook
+        Order[] storage orders = orderBook[_ticker][otype];
+        uint index = 0;
+        uint totalCost = 0;
+        uint totalToken = 0;
+        while(index < orders.length && _amount > 0){
+            Order storage order = orders[index];
+            uint buyerEth = balances[order.trader][ethTicker];
+            //this line checks whether the buyer has the mentioned amount of eth
+            if(buyerEth < order.amount.mul(order.price)){
+                index = index.add(1);
+                continue;
             }
-            balances[msg.sender][ethTicker]-= totalCost;
-            balances[msg.sender][_ticker]+= totalToken;
+            if(order.amount >= _amount){
+                if(buyerEth >= _amount.mul(order.price)){
+                    balances[order.trader][_ticker] = balances[order.trader][_ticker].add(_amount);
+                    balances[order.trader][ethTicker] = balances[order.trader][ethTicker].sub(_amount.mul(order.price));
+                    order.amount = order.amount.sub(_amount);
+                    totalCost = totalCost.add(_amount.mul(order.price));
+                    totalToken = totalToken.add(_amount);
+                    _amount = 0;
+                    break;
+                }
+            }
+            else{
+                if(buyerEth >= order.amount.mul(order.price)){
+                    balances[order.trader][_ticker] = balances[order.trader][_ticker].add(order.amount);
+                    balances[order.trader][ethTicker] = balances[order.trader][ethTicker].sub(order.amount.mul(order.price));
+                    _amount = _amount.sub(order.amount);
+                    totalCost = totalCost.add(order.amount.mul(order.price));
+                    totalToken = totalToken.add(order.amount);
+                    order.amount = 0;
+                }
+            }
+            index = index.add(1);
+            balances[msg.sender][_ticker] = balances[msg.sender][_ticker].sub(totalToken);
+            balances[msg.sender][ethTicker] = balances[msg.sender][ethTicker].add(totalCost);
             _delete(_ticker,otype);
         }
     }
-
+    function _executeSell(bytes32 _ticker,uint otype, uint _amount)private{
+        //msg.sender is buyer and seller is from sell side of the orderbook
+        Order[] storage orders = orderBook[_ticker][otype];
+        uint index = 0;
+        uint totalCost = 0;
+        uint totalToken = 0;
+        uint buyerEth = balances[msg.sender][ethTicker];
+        uint sellerToken = 0;
+        while(index < orders.length && _amount > 0){
+            Order storage order = orders[index];
+            sellerToken = balances[order.trader][_ticker];
+            //this line checks whether the seller has the mentioned amount of tokens or not
+            if(sellerToken < _amount){
+                index = index.add(1);
+                continue;
+            }
+            if(order.amount >= _amount){
+                if(buyerEth >= _amount * order.price){
+                    balances[order.trader][_ticker] = balances[order.trader][_ticker].sub(_amount);
+                    balances[order.trader][ethTicker] = balances[order.trader][ethTicker].add(_amount.mul(order.price));
+                    order.amount = order.amount.sub(_amount);
+                    totalCost = totalCost.add(_amount.mul(order.price));
+                    totalToken = totalToken.add(_amount);
+                    buyerEth = buyerEth.sub(_amount.mul(order.price));
+                    _amount = 0;
+                    break;
+                }
+            }
+            else{
+                if(buyerEth >= order.amount * order.price){
+                    balances[order.trader][_ticker] = balances[order.trader][_ticker].sub(order.amount);
+                    balances[order.trader][ethTicker] = balances[order.trader][ethTicker].add(order.amount.mul(order.price));
+                    _amount = _amount.sub(order.amount);
+                    totalCost = totalCost.add(order.amount.mul(order.price));
+                    totalToken = totalToken.add(order.amount);
+                    buyerEth = buyerEth.sub(order.amount.mul(order.price));
+                    order.amount = 0;
+                }
+            }
+            index = index.add(1);
+        }
+        balances[msg.sender][ethTicker] = balances[msg.sender][ethTicker].sub(totalCost);
+        balances[msg.sender][_ticker] = balances[msg.sender][_ticker].add(totalToken);
+        _delete(_ticker,otype);
+    }
     function _delete(bytes32 _ticker, uint _type)private{
         Order[] storage orders = orderBook[_ticker][_type];
         int index = -1;
